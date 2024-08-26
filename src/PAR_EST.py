0ccf213e3b1fc19eabaa3d53b88a406535ec08e3
Shahram Khorshidi <khorshidishahram@gmail.com>
2024-08-26 18:16:00 +0200

    add neural network

--- src/PAR_EST.py
+++ src/PAR_EST.py
@@ -12,7 +12,7 @@ dirPath = os.path.dirname(os.path.realpath(__file__))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 HID_DIM = 128
-LR = 1e-3
+LR = 1e-4
 
         
 class TORQUE_ESTIMATOR(nn.Module):
@@ -24,7 +24,7 @@ class TORQUE_ESTIMATOR(nn.Module):
         super(TORQUE_ESTIMATOR, self).__init__()
 
 
-        self.encoder_1 = nn.Sequential(
+        self.encoder = nn.Sequential(
                     nn.Linear(input_dim, HID_DIM),
                     nn.LeakyReLU(),
                     nn.Linear(HID_DIM, HID_DIM),
@@ -33,28 +33,25 @@ class TORQUE_ESTIMATOR(nn.Module):
                     nn.LeakyReLU(),
                     nn.Linear(HID_DIM, output_dim)
                     )
-        self.encoder_1.apply(self.init_weights)
+        self.encoder.apply(self.init_weights)
       
-
     def forward(self, state):
-        return self.encoder_1(state)
-
+        return self.encoder(state)
 
 
 class ESTIMATOR(nn.Module):
-    def __init__(self, input_dim, ouput_dim):
+    def __init__(self, input_dim, output_dim):
         super(ESTIMATOR, self).__init__()
         self.writer = SummaryWriter(dirPath+'/runs/Summary/'+"_EST_"+"_"+"L_DIM_"+str(HID_DIM)+"_"\
             +"_"+date)
 
-
-        self.est = TORQUE_ESTIMATOR(input_dim, ouput_dim).to(device)
+        self.est = TORQUE_ESTIMATOR(input_dim, output_dim).to(device)
         self.est_optimizer = optim.Adam(self.est.parameters(), lr=LR, weight_decay=1e-5)
         self.scheduler_e = optim.lr_scheduler.StepLR(self.est_optimizer, step_size=2000, gamma=0.1)
        
         self.train_step = 0
     
-    def train(self, target, input_data):
+    def train(self, input_data, target):
         pred_torque = self.est(input_data)
         loss = F.mse_loss(pred_torque, target)
         self.est_optimizer.zero_grad(set_to_none=True)
@@ -62,27 +59,21 @@ class ESTIMATOR(nn.Module):
         # torch.nn.utils.clip_grad_norm_(self.est.parameters(), CG) 
         self.est_optimizer.step()
         # write the loss to tensorboard
-        self.writer.add_scalar('MSE_loss',loss, self.train_step)
-            
-            
+        self.writer.add_scalar('MSE_loss/training',loss, self.train_step)
+        
         self.train_step += 1
 
-    def eval(self, target, input_data, epoch): # same as train, but without optimizer steps
+    def eval(self, input_data, target, epoch): # same as train, but without optimizer steps
         with torch.no_grad():
             pred_torque = self.est(input_data)
             loss = F.mse_loss(pred_torque, target)
-            self.writer.add_scalar('MSE_loss',loss, epoch)
+            self.writer.add_scalar('MSE_loss/evaluation',loss, epoch)
             #calculate l1 loss  
             l1_loss = F.l1_loss(pred_torque, target)
-            self.writer.add_scalar('L1_loss',l1_loss, epoch)
+            self.writer.add_scalar('L1_loss/evaluation',l1_loss, epoch)
            
-
- 
     def save(self, filename):
         torch.save(self.est.state_dict(), filename + "_est")
-
-
- 
 
     def load(self, filename):
         self.est.load_state_dict(torch.load(filename + "_est"))
